3fef129b265e9585941cbd296ac2b98f6c06c30a
demie4744 <demie4744@wrdsb.ca>
2020-09-16 16:28:31 +0000

    I got confused on the math, I didn't know if I should add, subtract, etc

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
  *
- * @author 
+ * @author Erkam
  */
 public class Main {
 
@@ -9,7 +10,32 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+     Scanner input = new Scanner(System.in);
+
+     final int SMALL_PIZZA = 10;
+     final int MEDIUM_PIZZA = 20;
+     final int LARGE_PIZZA = 40;
+     
+     
+     System.out.println("Enter Cm");
+     int Cm =input.nextInt();
+     
+      if (Cm >= SMALL_PIZZA ) {
+     System.out.println("We are going to make you a cute little pizza! And it will be 7.47");
+  
+     } else if (Cm >= MEDIUM_PIZZA ) {
+     System.out.println("This will be delicious and it will be 13.32");
+    } else 
+    System.out.println("“Whoa, big pizza! You might need a truck to get this home!");
     
+    
+
+     
+     
+    
+
+
+
+
   }
 }
